265397085ce57629b3401ef70b2ae8fa5938fc44
gitjcart <jshmccrt@gmail.com>
2017-09-10 16:12:43 -0400

    moved files lesson 1 to lesson1 directory

---
+++ lesson1/helloworld.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print('hello world')
+
+x = int(input('Enter an integer: '))
+if x%2 == 0:
+    print('Even')
+else:
+    print('Odd')
+print('Done with conditional')
