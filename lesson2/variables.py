7c9a8524f7926445ff231feeb36200290c0b9b7d
gitjcart <jshmccrt@gmail.com>
2017-09-11 10:20:38 -0400

    added variable.py, closed out lesson_1, opened lesson_2

---
+++ lesson2/variables.py
@@ -0,0 +1,19 @@
+# variable assignment
+
+# can't assign keywords to variables
+"""
+this isn't read by the interpreter
+this isn't read by the program
+this isn't read by the computer
+
+"""
+
+a=1
+b=2
+c=a+b
+
+print(a)
+print('')
+print(b)
+print('')
+print(c)
