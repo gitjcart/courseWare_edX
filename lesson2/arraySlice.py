dc295276407c3ce43a830b757fa9f01e590db4e2
gitjcart <jshmccrt@gmail.com>
2017-09-14 12:46:07 -0400

    added arraySlice.py

---
+++ lesson2/arraySlice.py
@@ -0,0 +1,13 @@
+# there is more than one way to slice an array, 
+# These are just examples of slicing
+
+
+bounce='high'
+pounce='far'
+mouse='mickey'
+house='casita'
+man='pendejo'
+
+combo=bounce + mouse
+
+print(combo)
