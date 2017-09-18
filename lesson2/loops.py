ff12e1f81346d44a3d3a8e1ee1ca1f48bbe55bca
gitjcart <jshmccrt@gmail.com>
2017-09-18 12:29:57 -0400

    added control flow to reflections. added 'while' loop to loops.py.

--- lesson2/loops.py
+++ lesson2/loops.py
@@ -7,7 +7,18 @@ Created on Mon Sep 18 11:17:39 2017
 """
 
 n = 0
-
 while n < 5:
     print(n)
-    n += 1
+    n += 1 # the '+=' is a shortcut operator to add to the variable
+
+o = 0
+while o < 20:
+    print(o)
+    o += 1  # the '+=' is a shortcut operator to add to the variable
+
+# this is an infinite loop if you always type 'right'
+# it will have nothing to evaluate to false.
+n = input("You are lost in a forest! Will you go left or right? --> ")
+while n == 'right':
+    n = input("You are lost in a forest! Will you go left or right? --> ")
+    print('You got out of the forest!')
