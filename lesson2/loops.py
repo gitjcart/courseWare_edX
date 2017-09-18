c4e2a480ca2377f8803db94d2e44984a8ee7f8e9
gitjcart <jshmccrt@gmail.com>
2017-09-18 12:38:01 -0400

    added for loop with an if/else statement.

--- lesson2/loops.py
+++ lesson2/loops.py
@@ -6,19 +6,36 @@ Created on Mon Sep 18 11:17:39 2017
 @author: jcart
 """
 
+
+
 n = 0
 while n < 5:
     print(n)
     n += 1 # the '+=' is a shortcut operator to add to the variable
 
+
+
 o = 0
 while o < 20:
     print(o)
     o += 1  # the '+=' is a shortcut operator to add to the variable
-
+    
+    
+"""
 # this is an infinite loop if you always type 'right'
 # it will have nothing to evaluate to false.
 n = input("You are lost in a forest! Will you go left or right? --> ")
 while n == 'right':
     n = input("You are lost in a forest! Will you go left or right? --> ")
     print('You got out of the forest!')
+"""
+
+
+
+# shortcut with 'for' loop
+for n in range(5):
+    print(n)
+if n == 4:
+    print('that\'s all buddy')
+else:
+    print(n)
