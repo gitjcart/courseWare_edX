63c05beca4d5838476da5f27bf5a44b4df965a2e
gitjcart <jshmccrt@gmail.com>
2017-10-01 18:48:04 -0400

    commented out with '#' signs vice """ and """

--- lesson2/loops.py
+++ lesson2/loops.py
@@ -1,127 +1,130 @@
-#!/usr/bin/env python3
+
+
+# !/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 18 11:17:39 2017
 
-@author: jcart
-"""
+# Created on Mon Sep 18 11:17:39 2017
 
-
-
-n = 0
-while n < 5:
-    print(n)
-    n += 1 # the '+=' is a shortcut operator to add to the variable
+# @author: jcart
 
 
 
-o = 0
-while o < 20:
-    print(o)
-    o += 1  # the '+=' is a shortcut operator to add to the variable
+
+# n = 0
+# while n < 5:
+#    print(n)
+#    n += 1 # the '+=' is a shortcut operator to add to the variable
+
+
+
+# o = 0
+# while o < 20:
+#    print(o)
+#    o += 1  # the '+=' is a shortcut operator to add to the variable
     
     
-"""
 # this is an infinite loop if you always type 'right'
 # it will have nothing to evaluate to false.
-n = input("You are lost in a forest! Will you go left or right? --> ")
-while n == 'right':
-    n = input("You are lost in a forest! Will you go left or right? --> ")
-    print('You got out of the forest!')
-"""
+# n = input("You are lost in a forest! Will you go left or right? --> ")
+# while n == 'right':
+#    n = input("You are lost in a forest! Will you go left or right? --> ")
+#    print('You got out of the forest!')
+
 
 
 # Intial FOR -- IN randge() statement
 
 # shortcut with 'for' loop
 
-for n in range(5):
-    print(n)
-if n == 4:
-    print('that\'s all buddy')
-else:
-    print(n)
-"""
-'for i in range(7, 10) calls on the numbers 
-in range 7-10. 'mysum += i' adds the numbers together
-'print(mysum) will print the added numbers.
-print(mysum) will print different values if located
-within the actual loop itself. it will print the total
-values as the loop goes through each sequence
-"""
-mysum = 0
-for i in range(7, 10):
-    mysum += i
-print(mysum)
+# for n in range(5):
+#   print(n)
+#    if n == 4:
+#        print('that\'s all buddy')
+#    else:
+#        print(n)
+
+# 'for i in range(7, 10) calls on the numbers 
+#in range 7-10. 'mysum += i' adds the numbers together
+# 'print(mysum) will print the added numbers.
+# print(mysum) will print different values if located
+# within the actual loop itself. it will print the total
+# values as the loop goes through each sequence
+
+# mysum = 0
+# for i in range(7, 10):
+#     mysum += i
+# print(mysum)
 
 # adding an if statement in 'for' loop
 
-thisSum = 0
-for i in range(0, 100, 15):
-    thisSum += i
-    print(thisSum)
-    if i < 15:
-        print(i)
+# thisSum = 0
+# for i in range(0, 100, 15):
+#     thisSum += i
+#     print(thisSum)
+#     if i < 15:
+#         print(i)
 #    elif i == 15:
 #        print(str(i, + " the number is ", i)
 #    elif i > 15:
 #        print("the number is larger than ", i)
-    else:
-        print("it has finished")
+#    else:
+#        print("it has finished")
 
 
 # the 'str()' function converts an 'int' to a string
 # so you can print to STDOUT by concatenating the string
 
-yoursum = 0
-for i in range(5, 11, 2):
-    yoursum += i
-    print(str(yoursum) + " i + i while in loop")
-print(str(yoursum) + " answer while not in loop")
+# yoursum = 0
+# for i in range(5, 11, 2):
+#     yoursum += i
+#     print(str(yoursum) + " i + i while in loop")
+# print(str(yoursum) + " answer while not in loop")
 
 # BREAK statement
-ourSum = 0
-for i in range(5, 11, 2):
-    ourSum += i
-    if ourSum == 5:
-        print(str(ourSum) + " this is a BREAK statement!")
-    break
+# ourSum = 0
+# for i in range(5, 11, 2):
+#     ourSum += i
+#     if ourSum == 5:
+#         print(str(ourSum) + " this is a BREAK statement!")
+#     break
 
 # break statement practice
 # DO NOT forget your colon after a for statment, an if statment, an elif statement, and an else statement!
-ourDifference = 0
-for i in range(1, 100):
-    ourDifference -= i
-    if ourDifference == -75:
-        print(str(ourDifference) + " this is an \'if\' statement within a loop!")
-    elif ourDifference == 1:
-        print(str(ourDifference) + " this was the \'elif\' statement within the \'while loop!\'")
-    elif ourDifference == -1:
-        print(str(ourDifference) + " this was the 2nd \'elif\' statement within the \'while loop!\'")
-    else:
-        print("this was the else statement in the \'while\' loop!")
-    break
+# ourDifference = 0
+# for i in range(1, 100):
+#     ourDifference -= i
+#     if ourDifference == -75:
+#         print(str(ourDifference) + " this is an \'if\' statement within a loop!")
+#     elif ourDifference == 1:
+#         print(str(ourDifference) + " this was the \'elif\' statement within the \'while loop!\'")
+#     elif ourDifference == -1:
+#         print(str(ourDifference) + " this was the 2nd \'elif\' statement within the \'while loop!\'")
+#     else:
+#         print("this was the else statement in the \'while\' loop!")
+#     break
 
-"""
-this is a practice WHILE LOOP assigned to a function.
 
-DO NOT FORGET YOUR COLON WHILE ASSIGNING A FUNCTION!!!!!
+# this is a practice WHILE LOOP assigned to a function.
 
-It appears we can't have a double argument in a function.
+# DO NOT FORGET YOUR COLON WHILE ASSIGNING A FUNCTION!!!!!
 
-The current way the program is written, it will not run..
+# It appears we can't have a double argument in a function.
 
-It returns a syntax error on the double 'int' argument
+# The current way the program is written, it will not run..
+
+# It returns a syntax error on the double 'int' argument
 
 ## WHY WOULD YOU NEED TWO ARGUMENTS?????!!!??
 
 ## YOU DON'T NEED ANY ARGUMENTS aka ZERO ARGUMENTS . . . watch. 
+
+
 """
-
-
 def i():
-    a=int(input())
-    b=int(input())
+#    a=int(input())
+#    b=int(input())
+    a = 1
+    b = 100
     ourDiff = 0
     for i in range(a, b):
         ourDiff -= i
@@ -142,6 +145,26 @@ def j():
     for i in range(5, 11):
         mysum += i
         if mysum == 5:
-            print(str(mysum) + " this is a BREAK statement!")
+            print(str(mysum) + " this is a BREAK statement in j() -- !")
         break
 j()
+"""
+
+def k():
+    myproduct = 1
+    for i in range(7, 19, 2):
+        myproduct *=  i
+        print(str(myproduct))
+        if myproduct == 63:
+            print(str(myproduct) + " This is another break statement in k() -- !")
+        elif myproduct ==7:
+            print(str(myproduct) + " This is another break statement in k() -- !")
+        elif myproduct == 693:
+            print(str(myproduct) + " This is another break statement in k() -- !")
+        elif myproduct == 9009:
+            print(str(myproduct) + " This is another break statement in k() -- !")
+        else:
+            break
+        print(str(myproduct))
+    print(str(myproduct))
+k()
