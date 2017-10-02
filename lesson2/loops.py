4f827a7aa03228fefcae587f9f6c0682b9bff6c2
gitjcart <jshmccrt@gmail.com>
2017-10-02 17:42:41 -0400

    commit to add a conditional statement four layers deep.

--- lesson2/loops.py
+++ lesson2/loops.py
@@ -104,7 +104,7 @@
 #     break
 
 
-# this is a practice WHILE LOOP assigned to a function.
+# this is a practice FOR LOOP assigned to a function.
 
 # DO NOT FORGET YOUR COLON WHILE ASSIGNING A FUNCTION!!!!!
 
@@ -174,6 +174,8 @@ def k():
         else:
             break
 k()
+print(" ")
+print(" ")
 
 # \
 # \
@@ -190,6 +192,8 @@ def l():
         print(str(myQuotient) + " this is the second \'myQuotient\'!")
     print(str(myQuotient) + " this is the \'out of loop\' \'myQuotient\'!") 
 l()
+print(" ")
+print(" ")
 
 # \
 # \
@@ -204,8 +208,7 @@ def m():
     myDifference = 0
     myQuotient = -0.1
 #    print "%s, %s, %s, %s," % (mySum, myProduct, myDifference, myQuotient,)
-    print(mySum, myProduct, myDifference, myQuotient)
-    for i in range(1, 20, 2):
+    for i in range(1, 9, 2):
         mySum += i
         myProduct *= i
         myDifference -= i
@@ -213,6 +216,44 @@ def m():
         if myProduct == 1:
             print(str(myProduct) + " this was the if for myProduct")
             if mySum == 1:
-                break
-        print(mySum, myProduct, myDifference, myQuotient)
+                print(str(mySum) + " this was the if for mySum")
+                if myDifference == -1:
+                    print(str(myDifference) + " this was the if for myDifference")
+                    if myQuotient == -0.1:
+                        print(str(myQuotient) + " this was the if for myQuotient"), print(" ")
+        elif myProduct == 3:
+            print(str(myProduct) + " this was the 2nd if for myProduct")
+            if mySum == 4:
+                print(str(mySum) + " this was the 2nd if for mySum")
+                if myDifference == -4:
+                    print(str(myDifference) + " this was the 2nd if for myDifference")
+                    if myQuotient == -0.03333333333333333:
+                        print(str(myQuotient) + " this was the 2nd if for myQuotient"), print(" ")
+        elif myProduct == 15:
+            print(str(myProduct) + " this was the 3rd if for myProduct")
+            if mySum == 9:
+                print(str(mySum) + " this was the 3rd if for mySum")
+                if myDifference == -9:
+                    print(str(myDifference) + " this was the 3rd if for myDifference")
+                    if myQuotient == -0.006666666666666666:
+                        print(str(myQuotient) + " this was the 3rd if for myQuotient"), print(" ")
+        elif myProduct == 105:
+            print(str(myProduct) + " this was the 4th if for myProduct")
+            if mySum == 16:
+                print(str(mySum) + " this was the 4th if for mySum")
+                if myDifference == -16:
+                    print(str(myDifference) + " this was the 4th if for myDifference")
+                    if myQuotient == -0.0009523809523809523:
+                        print(str(myQuotient) + " this was the 4th if for myQuotient"), print(" ")
+        elif myProduct == 945:
+            print(str(myProduct) + " this was the 5th if for myProduct")
+            if mySum == 25:
+                print(str(mySum) + " this was the 5th if for mySum")
+                if myDifference == -25:
+                    print(str(myDifference) + " this was the 5th if for myDifference")
+                    if myQuotient == -0.00010582010582010581:
+                        print(str(myQuotient) + " this was the 5th if for myQuotient"), print(" ")
+            print("blimey got it wrong, Josh")
+#            break
+#        print(myProduct, mySum, myDifference, myQuotient)
 m()
