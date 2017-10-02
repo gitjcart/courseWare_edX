f7e1587fbd6ee48557a266cd68fd898623dc4eb0
gitjcart <jshmccrt@gmail.com>
2017-10-02 18:19:09 -0400

    added the incorrect version for training purposes.

--- lesson2/correctShortcut.py
+++ lesson2/correctShortcut.py
@@ -4,3 +4,21 @@
 
 x = "Hello"
 print ("Hello World! %s" %(x))
+
+
+
+# HERE IS THE WRONG FORMAT. where x is a string. and print() is a NoneType.
+# The NoneType doesnt return anything, when it tries to assign x=None
+# it pulls an error
+
+
+# x = "Hello"
+# print ("Hello World! %s") %(x)
+
+# Raises the following error:
+
+# Hello World! %s
+# Traceback (most recent call last):
+#	 File "main.py", line 3, in 
+#	 print ("Hello World! %s") %(x)
+# TypeError: unsupported operand type(s) for %: 'NoneType' and 'str'
