b0f03fd30d3832bb39dcba838b8354bf0f9636f9
gitjcart <jshmccrt@gmail.com>
2017-10-06 13:32:19 -0400

    created p.py as a user input/conditional mini-program

---
+++ lesson2/p.py
@@ -0,0 +1,11 @@
+# p.py
+
+print('')
+
+a = input("what is your favorite animal? ---> ")
+print('')
+b = input("What is your favorite desination? ---> ")
+print('')
+c = int(input("What is your favorite number? ---> "))
+
+print("your favorite number is %s, you like traveling %s, and you enjoy %s" %(c, a, b,))
