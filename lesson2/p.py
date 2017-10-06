36c132743501a381a8b1ce47a5422ca68739c90e
gitjcart <jshmccrt@gmail.com>
2017-10-06 17:25:00 -0400

    modified a variable to represent a math operation[training]

--- lesson2/p.py
+++ lesson2/p.py
@@ -1,7 +1,8 @@
 # p.py
 
+# this represents a newline, as i am unsure how to do that otherwise
 print('')
-
+# takes input from the user
 a = input("what is your favorite animal? ---> ")
 print('')
 b = input("What is your favorite desination? ---> ")
@@ -9,3 +10,12 @@ print('')
 c = int(input("What is your favorite number? ---> "))
 
 print("your favorite number is %s, you like traveling %s, and you enjoy %s" %(c, a, b,))
+
+
+# a is set equal to the mathematical operation 11 minus 20
+# it saves the difference of the two numbers as a variable.
+a = [11-20
+
+]
+
+print(a)
