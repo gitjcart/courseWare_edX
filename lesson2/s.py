866af7e25ab2e77a2654f6ec742400e568ce9be9
gitjcart <jshmccrt@gmail.com>
2017-10-07 09:34:06 -0400

    tell me what is wrong with this set of code

---
+++ lesson2/s.py
@@ -0,0 +1,7 @@
+# s.py
+print(a)
+print("~/Documents/courseWare_edX/lesson2/s.py")
+print("this is the current directory ")
+a = input("what did you think of the course? ---> ")
+
+print(" Thank you for your feedback! ")
